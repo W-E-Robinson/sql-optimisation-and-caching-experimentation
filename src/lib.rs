2701b2b2b9051b6dadf7242eb78030f25db39b27
Will Robinson <william.ellis.robinson@gmail.com>
2025-02-16 09:44:09 +0000

    feat: Insertion of accounts rows
    including audit_logs rows

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use core::panic;
+
 // NOTE: lib.rs? or another file? - as not a library
 // think on general repo org oo
 use chrono::{DateTime, Duration, Utc};
@@ -5,6 +7,7 @@ use fake::faker::internet::en::{SafeEmail, Username};
 use fake::faker::name::{en::FirstName, en::LastName};
 use fake::faker::phone_number::en::PhoneNumber;
 use fake::Fake;
+use rand::Rng;
 use sqlx::{Pool, Postgres};
 use uuid::Uuid;
 
@@ -18,14 +21,15 @@ pub struct UserRowAccount {
     created_at: DateTime<Utc>,
 }
 
-// pub struct AccountRowInsertion {
-//     user_id: i32,
-//     account_type: String,
-//     balance: f64,
-//     created_at: DateTime<Utc>,
-//     num_active_cards: i32,
-// }
+pub struct AccountRowInsertion {
+    user_id: i32,
+    account_type: String,
+    balance: f64,
+    created_at: DateTime<Utc>,
+    num_active_cards: i32,
+}
 
+#[derive(PartialEq)]
 enum AuditLogAction {
     UserCreated,
     UserUpdated,
@@ -44,6 +48,24 @@ enum AuditLogAction {
     AccountCreated,
     AccountUpdated,
     AccountDeleted,
+}
+
+enum AccountType {
+    Checking,
+    Savings,
+    Credit,
+    Business,
+}
+
+impl AccountType {
+    fn to_string(&self) -> String {
+        match self {
+            Self::Checking => String::from("checking"),
+            Self::Savings => String::from("savings"),
+            Self::Credit => String::from("credit"),
+            Self::Business => String::from("business"),
+        }
+    }
 }
 
 impl AuditLogAction {
@@ -66,6 +88,32 @@ impl AuditLogAction {
             Self::AccountCreated => "account created",
             Self::AccountUpdated => "account updated",
             Self::AccountDeleted => "account deleted",
+        }
+    }
+
+    fn from_string(action: &str) -> Self {
+        match action {
+            "user created" => Self::UserCreated,
+            "user updated" => Self::UserUpdated,
+            "user deleted" => Self::UserDeleted,
+            "transfer created" => Self::TransferCreated,
+            "transfer updated" => Self::TransferUpdated,
+            "transaction created" => Self::TransactionCreated,
+            "transaction deleted" => Self::TransactionDeleted,
+            "payment created" => Self::PaymentCreated,
+            "payment updated" => Self::PaymentUpdated,
+            "loan created" => Self::LoanCreated,
+            "loan updated" => Self::LoanUpdated,
+            "card created" => Self::CardCreated,
+            "card updated" => Self::CardUpdated,
+            "card deleted" => Self::CardDeleted,
+            "account created" => Self::AccountCreated,
+            "account updated" => Self::AccountUpdated,
+            "account deleted" => Self::AccountDeleted,
+            _ => panic!(
+                "Error: AuditLogAction not found from action - <action = {}>",
+                action
+            ), // NOTE: or None?
         }
     }
 }
@@ -116,9 +164,9 @@ impl BankSystemManager {
         .await
         {
             println!(
-                "Error: failed to insert row into 'audit_logs' - <user_id = {}> - <action = {}> - <error = {:?}>",
-                user_id, action, e
-            );
+                 "Error: failed to insert row into 'audit_logs' - <user_id = {}> - <action = {}> - <error = {:?}>",
+                 user_id, action, e
+             );
         }
     }
 
@@ -175,70 +223,86 @@ impl BankSystemManager {
         }
     }
 
-    // async fn insert_accounts(&self) {
-    //     let mut accounts_count = 1;
-    //     loop {
-    //         if accounts_count > 2000 {
-    //             break;
-    //         }
+    async fn insert_accounts(&self) {
+        let mut accounts_count = 1;
+        let mut current_user_id = 1;
+        let mut accounts_per_user = 0;
+        loop {
+            if accounts_count > 4000 {
+                break;
+            }
 
-    //         let created_at = self.random_date(9, 8);
+            let account_type = match accounts_per_user {
+                0 => AccountType::Checking,
+                1 => AccountType::Savings,
+                2 => AccountType::Credit,
+                3 => AccountType::Business,
+                _ => AccountType::Checking,
+            };
+            let created_at = self.random_date(9, 8);
+            let num_active_cards = match account_type {
+                AccountType::Checking => 1,
+                AccountType::Savings => 0,
+                AccountType::Credit => 1,
+                AccountType::Business => 2,
+            };
 
-    //         let account = AccountRowInsertion {
-    //             user_id: accounts_count % 3,
-    //             account_type: 'something' one of each?,
-    //             balance: random number,
-    //             created_at,
-    //             num_active_cards: 2, depends on type right?
-    //         };
-    //         if let Err(e) = sqlx::query(
-    //             "
-    //             insert into public.accounts
-    //             (user_id, account_type, balance, created, num_active_cards)
-    //             values ($1, $2, $3, $4, $5);
-    //             ",
-    //         )
-    //         .bind(account.user_id)
-    //         .bind(account.account_type)
-    //         .bind(account.balance)
-    //         .bind(account.created_at)
-    //         .bind(account.num_active_cards)
-    //         .execute(&self.db)
-    //         .await
-    //         {
-    //             println!(
-    //                 "Error: failed to insert row into 'accounts' - <id = {}> - <error = {:?}>",
-    //                 accounts_count, e
-    //             );
-    //         } else {
-    //             self.insert_audit_log(
-    //                 accounts_count,
-    //                 AuditLogAction::AccountCreated.to_string(),
-    //                 format!("account_id id <{}>", accounts_count),
-    //                 created_at,
-    //             )
-    //             .await
-    //         }
+            let account = AccountRowInsertion {
+                user_id: current_user_id,
+                account_type: account_type.to_string(),
+                balance: format!("{:.2}", rand::rng().random_range(0..=1_000_000))
+                    .parse()
+                    .unwrap_or(0.00),
+                created_at,
+                num_active_cards,
+            };
+            if let Err(e) = sqlx::query(
+                "
+                insert into public.accounts
+                (user_id, account_type, balance, created_at, num_active_cards)
+                values ($1, $2, $3, $4, $5);
+                ",
+            )
+            .bind(account.user_id)
+            .bind(account.account_type)
+            .bind(account.balance)
+            .bind(account.created_at)
+            .bind(account.num_active_cards)
+            .execute(&self.db)
+            .await
+            {
+                println!(
+                    "Error: failed to insert row into 'accounts' - <id = {}> - <error = {:?}>",
+                    accounts_count, e
+                );
+            } else {
+                self.insert_audit_log(
+                    current_user_id,
+                    AuditLogAction::AccountCreated.to_string(),
+                    format!("account id <{}>", accounts_count),
+                    created_at,
+                )
+                .await
+            }
 
-    //         accounts_count += 1;
-    //     }
-    // }
+            accounts_count += 1;
+            accounts_per_user += 1;
+            if accounts_per_user == 4 {
+                accounts_per_user = 0;
+                current_user_id += 1;
+            }
+        }
+    }
 
     async fn insert_data(&self) {
         self.insert_users().await;
-        // self.insert_accounts().await;
+        self.insert_accounts().await;
     }
-}
-
-fn main() {
-    // BankSystemManager.build_schema() = not needed as in a docker file? = even needed if all
-    // testing?
-    // BankSystemManager.insert_data().await
 }
 
 #[cfg(test)]
 mod test {
-    use sqlx::PgPool;
+    use sqlx::{PgPool, Row};
 
     use super::*;
 
@@ -260,6 +324,51 @@ mod test {
 
         assert_eq!(users.len(), 1000);
         assert_eq!(audit_logs.len(), 1000);
+        let user_insertions: Vec<_> = audit_logs
+            .iter()
+            .filter(|log| {
+                AuditLogAction::from_string(log.get::<String, _>("action").as_str())
+                    == AuditLogAction::UserCreated
+            })
+            .collect();
+        assert_eq!(user_insertions.len(), 1000);
+
+        Ok(())
+    }
+
+    #[sqlx::test(fixtures(
+        "../db/schema/users.sql",
+        "../db/schema/accounts.sql",
+        "../db/schema/audit_logs.sql"
+    ))] // NOTE: check num cards + type of accounts?
+    async fn test_accounts_created(pool: PgPool) -> sqlx::Result<()> {
+        let bank_system_manager = BankSystemManager::new(pool.clone());
+
+        bank_system_manager.insert_users().await;
+        bank_system_manager.insert_accounts().await;
+
+        let mut conn = pool.acquire().await?;
+
+        let accounts = sqlx::query("SELECT * FROM public.accounts")
+            .fetch_all(&mut *conn)
+            .await?;
+
+        let audit_logs = sqlx::query("SELECT * FROM public.audit_logs")
+            .fetch_all(&mut *conn)
+            .await?;
+
+        assert_eq!(accounts.len(), 4000);
+        assert_eq!(audit_logs.len(), 5000); // NOTE: need to clear down audits between tests? -
+                                            // also after all to allow tests run on after another
+                                            // no docker restar
+        let account_insertions: Vec<_> = audit_logs
+            .iter()
+            .filter(|log| {
+                AuditLogAction::from_string(log.get::<String, _>("action").as_str())
+                    == AuditLogAction::AccountCreated
+            })
+            .collect();
+        assert_eq!(account_insertions.len(), 4000);
 
         Ok(())
     }
